17f438d2b3674dd669f0378aaaee1109d7968744
Bravo1B9 <brofe93@gmail.com>
2023-03-27 17:05:46 +0000

    Implement database connection

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const { connectToDb, getDb } = require('./db')
 
 const app = express();
 
+let db;
+connectToDb((err) => {
+  if(!err) {
+    app.listen(3000, () => {
+      console.log('Listening in port 3000');
+    });
+    db = getDb();
+  }
+});
+
 app.get('/books', (req, res) => {
   res.json({ message: 'All the books' });
-})
-
-app.listen(3000, () => {
-  console.log('Listening in port 3000');
 });
